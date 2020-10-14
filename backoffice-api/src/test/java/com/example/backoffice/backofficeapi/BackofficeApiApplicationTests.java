cafcb7bd54e02023e2f6069778e08c5d61ecdb60
horndaneth <horndaneth3@gmail.com>
2020-10-14 11:47:07 +0700

    #add child modules.

---
+++ backoffice-api/src/test/java/com/example/backoffice/backofficeapi/BackofficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backoffice.backofficeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
